8eca104bf51e0c7de0fc1c289c1c98c403d564a0
Muhammad Taufik Hidayat <wmkbisa@gmail.com>
2023-05-10 22:13:59 +0700

    add route

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ app = Flask(__name__,
             )
 
 # app.add_url_rule('/predict', 'predict', views.predictpage, methods=['POST', 'GET'])
-
+@app.route("/")
 app.add_url_rule('/base','base',views.base)
 app.add_url_rule('/','index',views.index)
 app.add_url_rule('/websiteapp','websiteapp',views.websiteapp)
 app.add_url_rule('/website','website',views.website,methods=['GET','POST'])
-@app.route("/")
+
 if __name__ == "__main__":
 	warnings.filterwarnings("ignore", category=DeprecationWarning)
 	run_with_ngrok(app)
