ed2544561d1a4e06fc7aa72eeed7b03981696f2b
Muhammad Taufik Hidayat <wmkbisa@gmail.com>
2023-05-11 10:56:24 +0700

    edit main.py

--- main.py
+++ main.py
@@ -11,13 +11,26 @@ app = Flask(__name__,
             )
 
 # app.add_url_rule('/predict', 'predict', views.predictpage, methods=['POST', 'GET'])
-@app.route("/")
-app.add_url_rule('/base','base',views.base)
-app.add_url_rule('/','index',views.index)
-app.add_url_rule('/websiteapp','websiteapp',views.websiteapp)
-app.add_url_rule('/website','website',views.website,methods=['GET','POST'])
 
-if __name__ == "__main__":
-	warnings.filterwarnings("ignore", category=DeprecationWarning)
-	run_with_ngrok(app)
-	app.run()
+# app.add_url_rule('/base','base',views.base)
+# app.add_url_rule('/','index',views.index)
+# app.add_url_rule('/websiteapp','websiteapp',views.websiteapp)
+# app.add_url_rule('/website','website',views.website,methods=['GET','POST'])
+
+@app.route('/base','base',views.base)
+@app.route('/','index',views.index)
+def index():
+    if __name__ == "__main__":
+        warnings.filterwarnings("ignore", category=DeprecationWarning)
+        run_with_ngrok(app)
+        app.run()
+    return 'Web Object Detection'
+        
+@app.route('/websiteapp','websiteapp',views.websiteapp)
+def websiteapp():
+    return 'WebsiteApp'
+
+
+@app.route('/website','website',views.website,methods=['GET','POST'])
+def website():
+    return 'Detection Process'
