296410de676820758067d7aae26b1815bfe38165
Muhammad Taufik Hidayat <wmkbisa@gmail.com>
2023-05-11 15:20:43 +0700

    deploy test

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, url_for
+import requests
+import configparser
+from flask import Flask, url_for, request, render_template
 from app import views
 from flask_ngrok import run_with_ngrok
 import tensorflow as tf
@@ -12,30 +14,34 @@ app = Flask(__name__,
 
 # app.add_url_rule('/predict', 'predict', views.predictpage, methods=['POST', 'GET'])
 
-app.add_url_rule('/base','base',views.base)
-app.add_url_rule('/','index',views.index)
-app.add_url_rule('/websiteapp','websiteapp',views.websiteapp)
-app.add_url_rule('/website','website',views.website,methods=['GET','POST'])
+# app.add_url_rule('/base','base',views.base)
+# app.add_url_rule('/','index',views.index)
+# app.add_url_rule('/websiteapp','websiteapp',views.websiteapp)
+# app.add_url_rule('/website','website',views.website,methods=['GET','POST'])
+
+# if __name__ == "__main__":
+#         warnings.filterwarnings("ignore", category=DeprecationWarning)
+#         run_with_ngrok(app)
+#         app.run()
+
+@app.route('/base','base',views.base)
+def base():
+    return render_template('base.html')
+
+@app.route('/','index',views.index)
+def index():
+    return render_template('index.html')
+        
+@app.route('/websiteapp','websiteapp',views.websiteapp)
+def websiteapp():
+    return render_template('websiteapp.html')
+
+
+@app.route('/website','website',views.website,methods=['GET','POST'])
+def website():
+    return render_template('website.html')
 
 if __name__ == "__main__":
         warnings.filterwarnings("ignore", category=DeprecationWarning)
         run_with_ngrok(app)
         app.run()
-
-# @app.route('/base')
-# @app.route('/')
-# def index():
-#     if __name__ == "__main__":
-#         warnings.filterwarnings("ignore", category=DeprecationWarning)
-#         run_with_ngrok(app)
-#         app.run()
-#     return 'Web Object Detection'
-        
-# @app.route('/websiteapp')
-# def websiteapp():
-#     return 'WebsiteApp'
-
-
-# @app.route('/website')
-# def website():
-#     return 'Detection Process'
