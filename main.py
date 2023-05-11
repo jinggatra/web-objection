f2891eb884fd0f1aaeb7172b35c742e537dfd0eb
Muhammad Taufik Hidayat <wmkbisa@gmail.com>
2023-05-11 15:35:24 +0700

    backup and edit

--- main.py
+++ main.py
@@ -14,34 +14,34 @@ app = Flask(__name__,
 
 # app.add_url_rule('/predict', 'predict', views.predictpage, methods=['POST', 'GET'])
 
-# app.add_url_rule('/base','base',views.base)
-# app.add_url_rule('/','index',views.index)
-# app.add_url_rule('/websiteapp','websiteapp',views.websiteapp)
-# app.add_url_rule('/website','website',views.website,methods=['GET','POST'])
+app.add_url_rule('/base','base',views.base)
+app.add_url_rule('/','index',views.index)
+app.add_url_rule('/websiteapp','websiteapp',views.websiteapp)
+app.add_url_rule('/website','website',views.website,methods=['GET','POST'])
+
+if __name__ == "__main__":
+        warnings.filterwarnings("ignore", category=DeprecationWarning)
+        run_with_ngrok(app)
+        app.run(host="0.0.0.0", port=5000)
+
+# @app.route('/base','base',views.base)
+# def base():
+#     return render_template('base.html')
+
+# @app.route('/','index',views.index)
+# def index():
+#     return render_template('index.html')
+        
+# @app.route('/websiteapp','websiteapp',views.websiteapp)
+# def websiteapp():
+#     return render_template('websiteapp.html')
+
+
+# @app.route('/website','website',views.website,methods=['GET','POST'])
+# def website():
+#     return render_template('website.html')
 
 # if __name__ == "__main__":
 #         warnings.filterwarnings("ignore", category=DeprecationWarning)
 #         run_with_ngrok(app)
 #         app.run()
-
-@app.route('/base','base',views.base)
-def base():
-    return render_template('base.html')
-
-@app.route('/','index',views.index)
-def index():
-    return render_template('index.html')
-        
-@app.route('/websiteapp','websiteapp',views.websiteapp)
-def websiteapp():
-    return render_template('websiteapp.html')
-
-
-@app.route('/website','website',views.website,methods=['GET','POST'])
-def website():
-    return render_template('website.html')
-
-if __name__ == "__main__":
-        warnings.filterwarnings("ignore", category=DeprecationWarning)
-        run_with_ngrok(app)
-        app.run()
